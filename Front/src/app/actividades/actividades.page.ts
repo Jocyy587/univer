02d668be017e4b87275efd9a449ea7146f82d4ef
Your Name <you@example.com>
2025-11-10 17:29:21 -0600

    Todo el front

--- Front/src/app/actividades/actividades.page.ts
+++ Front/src/app/actividades/actividades.page.ts
@@ -1,19 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonAccordionGroup, IonAccordion, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-actividades',
   templateUrl: './actividades.page.html',
   styleUrls: ['./actividades.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonAccordionGroup, IonAccordion, IonItem, IonLabel, CommonModule, FormsModule]
 })
-export class ActividadesPage implements OnInit {
+export class ActividadesPage implements OnInit, AfterViewInit {
+  @ViewChild('donutCanvas') private donutCanvas?: ElementRef;
+
+  donutChart: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.createDonutChart();
+  }
+
+  createDonutChart() {
+    Chart.register(...registerables);
+    if (!this.donutCanvas) return;
+
+    this.donutChart = new Chart(this.donutCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['Completadas', 'En Progreso', 'Pendientes'],
+        datasets: [{
+          data: [12, 19, 5], // Datos de ejemplo
+          backgroundColor: [
+            'rgba(90, 156, 0, 1)',   // Verde sólido
+            'rgba(211, 84, 0, 1)',   // Naranja sólido
+            'rgba(140, 109, 54, 1)' // Amarillo/Dorado sólido
+          ],
+          borderColor: [
+            'rgba(90, 156, 0, 1)',
+            'rgba(211, 84, 0, 1)',
+            'rgba(140, 109, 54, 1)'
+          ],
+          borderWidth: 3 // Un borde un poco más grueso
+        }]
+      },
+      options: {
+        responsive: true,
+        cutout: '70%',
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
   }
 }
